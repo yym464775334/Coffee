9d97e29f78da2db51ebb1f7f402c17ae568c2f78
yym <464775334@qq.com>
2020-01-15 22:03:15 +0800

    提交

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ console.log(move5);
 var key = true;
 wra.onmouseenter = function (e) {
   // wra.style.animationPlayState = 'paused';
-  move1.style.animation = 'move1 2s linear infinite';
-  move2.style.animation = 'move2 2s linear infinite';
-  move3.style.animation = 'move3 2s linear infinite';
-  move4.style.animation = 'move4 2s linear infinite';
-  move5.style.animation = 'move5 2s linear infinite';
-  move6.style.animation = 'move6 2s linear infinite';
+  move1.style.animation = 'move1 4s linear infinite';
+  move2.style.animation = 'move2 4s linear infinite';
+  move3.style.animation = 'move3 4s linear infinite';
+  move4.style.animation = 'move4 4s linear infinite';
+  move5.style.animation = 'move5 4s linear infinite';
+  move6.style.animation = 'move6 4s linear infinite';
 
 }
 wra.onmouseleave = function (e) {
